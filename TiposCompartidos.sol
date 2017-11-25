bc6188fd6cd7c988ba4bb26213333c8b2eefe87d
empresarioMadri <32299993+empresarioMadri@users.noreply.github.com>
2017-11-25 19:18:24 +0100

    cambio

--- TiposCompartidos.sol
+++ TiposCompartidos.sol
@@ -40,5 +40,22 @@ library TiposCompartidos {
         uint fechaCreacion;
         uint polenes;
     }
+
+    function safeMul(uint a, uint b) internal pure returns (uint) {
+        uint c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
+    }
+
+    function safeSub(uint a, uint b) internal pure returns (uint) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function safeAdd(uint a, uint b) internal pure returns (uint) {
+        uint c = a + b;
+        assert(c>=a && c>=b);
+        return c;
+    }
     
 }
